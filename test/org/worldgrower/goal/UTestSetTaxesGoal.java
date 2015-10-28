65cf1f54c61f8b4ecf6b34bccf3e19bea6793080
Tom <Tom@84.198.73.100>
2015-10-28 14:52:03 +0100

    add unit tests

---
+++ test/org/worldgrower/goal/UTestSetTaxesGoal.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+package org.worldgrower.goal;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.worldgrower.Constants;
+import org.worldgrower.TestUtils;
+import org.worldgrower.World;
+import org.worldgrower.WorldImpl;
+import org.worldgrower.WorldObject;
+import org.worldgrower.actions.Actions;
+import org.worldgrower.attribute.WorldObjectContainer;
+
+public class UTestSetTaxesGoal {
+
+	private SetTaxesGoal goal = Goals.SET_TAXES_GOAL;
+	
+	@Test
+	public void testCalculateGoalSetTaxRate() {
+		World world = new WorldImpl(0, 0, null, null);
+		WorldObject performer = createPerformer();
+		
+		assertEquals(Actions.SET_TAX_RATE_ACTION, goal.calculateGoal(performer, world).getManagedOperation());
+	}
+
+	@Test
+	public void testIsGoalMet() {
+		World world = new WorldImpl(10, 10, null, null);
+		WorldObject performer = createPerformer();
+		world.addWorldObject(performer);
+		
+		WorldObject organization = GroupPropertyUtils.createVillagersOrganization(world);
+		organization.setProperty(Constants.ID, 1);
+		world.addWorldObject(organization);
+		assertEquals(true, goal.isGoalMet(performer, world));
+		
+		
+		organization.setProperty(Constants.ORGANIZATION_LEADER_ID, performer.getProperty(Constants.ID));
+		assertEquals(false, goal.isGoalMet(performer, world));
+		
+		organization.setProperty(Constants.SHACK_TAX_RATE, 1);
+		organization.setProperty(Constants.HOUSE_TAX_RATE, 2);
+		assertEquals(true, goal.isGoalMet(performer, world));
+	}
+
+	private WorldObject createPerformer() {
+		WorldObject performer = TestUtils.createSkilledWorldObject(7, Constants.INVENTORY, new WorldObjectContainer());
+		performer.setProperty(Constants.X, 0);
+		performer.setProperty(Constants.Y, 0);
+		performer.setProperty(Constants.WIDTH, 1);
+		performer.setProperty(Constants.HEIGHT, 1);
+		return performer;
+	}
+}
