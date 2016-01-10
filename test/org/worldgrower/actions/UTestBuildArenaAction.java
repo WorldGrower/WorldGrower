d7448cf3f0191e1cec76a0854247776570ae5c67
Tom <Tom@Tom_PC>
2016-01-10 14:33:51 +0100

    add unit tests

--- test/org/worldgrower/actions/UTestBuildArenaAction.java
+++ test/org/worldgrower/actions/UTestBuildArenaAction.java
@@ -24,6 +24,7 @@ import org.worldgrower.WorldImpl;
 import org.worldgrower.WorldObject;
 import org.worldgrower.attribute.IdList;
 import org.worldgrower.attribute.WorldObjectContainer;
+import org.worldgrower.generator.Item;
 
 public class UTestBuildArenaAction {
 
@@ -36,6 +37,24 @@ public class UTestBuildArenaAction {
 		
 		assertEquals(6, world.getWorldObjects().size());
 		assertEquals("Arena vertical", world.getWorldObjects().get(0).getProperty(Constants.NAME));
+	}
+	
+	@Test
+	public void testIsValidTarget() {
+		World world = new WorldImpl(0, 0, null, null);
+		WorldObject performer = createPerformer(2);
+		WorldObject target = createPerformer(3);
+		assertEquals(true, Actions.BUILD_ARENA_ACTION.isValidTarget(performer, target, world));
+	}
+	
+	@Test
+	public void testDistance() {
+		World world = new WorldImpl(0, 0, null, null);
+		WorldObject performer = createPerformer(2);
+		WorldObject target = createPerformer(3);
+		performer.getProperty(Constants.INVENTORY).addQuantity(Item.STONE.generate(1f), 10);
+		
+		assertEquals(0, Actions.BUILD_ARENA_ACTION.distance(performer, target, new int[0], world));
 	}
 	
 	private WorldObject createPerformer(int id) {
