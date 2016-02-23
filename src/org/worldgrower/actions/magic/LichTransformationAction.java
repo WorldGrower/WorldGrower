3a382796fdfe82402a5a8b78adc3a5b956cf6079
Tom <Tom@Tom_PC>
2016-02-23 14:43:16 +0100

    undead creatures can't turn into liches

--- src/org/worldgrower/actions/magic/LichTransformationAction.java
+++ src/org/worldgrower/actions/magic/LichTransformationAction.java
@@ -25,6 +25,7 @@ import org.worldgrower.attribute.SkillProperty;
 import org.worldgrower.attribute.SkillUtils;
 import org.worldgrower.attribute.WorldObjectContainer;
 import org.worldgrower.condition.LichUtils;
+import org.worldgrower.creaturetype.CreatureType;
 import org.worldgrower.goal.MagicSpellUtils;
 import org.worldgrower.gui.ImageIds;
 
@@ -44,7 +45,7 @@ public class LichTransformationAction implements MagicSpell {
 	
 	@Override
 	public boolean isValidTarget(WorldObject performer, WorldObject target, World world) {
-		return CraftUtils.isValidTarget(performer, target, world) && MagicSpellUtils.canCast(performer, this);
+		return CraftUtils.isValidTarget(performer, target, world) && (performer.getProperty(Constants.CREATURE_TYPE) != CreatureType.UNDEAD_CREATURE_TYPE) && MagicSpellUtils.canCast(performer, this);
 	}
 
 	@Override
