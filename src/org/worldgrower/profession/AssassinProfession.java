c6839bcd938b20a1cd8fa7624d87a94312bfd4e6
Tom <Tom@Tom_PC>
2016-03-19 13:34:25 +0100

    add assassinprofession

---
+++ src/org/worldgrower/profession/AssassinProfession.java
@@ -0,0 +1,68 @@
+/*******************************************************************************
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+package org.worldgrower.profession;
+
+import java.io.ObjectStreamException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.worldgrower.Constants;
+import org.worldgrower.attribute.SkillProperty;
+import org.worldgrower.generator.Item;
+import org.worldgrower.goal.Goal;
+import org.worldgrower.goal.Goals;
+
+public class AssassinProfession implements Profession {
+
+	public AssassinProfession(List<Profession> allProfessions) {
+		allProfessions.add(this);
+	}
+
+	@Override
+	public String getDescription() {
+		return "assassin";
+	}
+
+	@Override
+	public List<Goal> getProfessionGoals() {
+		return Arrays.asList(
+				Goals.EQUIPMENT_GOAL
+		);
+	}
+
+	@Override
+	public SkillProperty getSkillProperty() {
+		return Constants.ONE_HANDED_SKILL;
+	}
+	
+	public Object readResolve() throws ObjectStreamException {
+		return readResolveImpl();
+	}
+
+	@Override
+	public boolean isPaidByVillagerLeader() {
+		return false;
+	}
+
+	@Override
+	public boolean avoidEnemies() {
+		return false;
+	}
+	
+	@Override
+	public List<Item> getSellItems() {
+		return Arrays.asList();
+	}
+}
