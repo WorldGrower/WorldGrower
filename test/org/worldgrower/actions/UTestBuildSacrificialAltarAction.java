7a777369e41aeaddeb81e9bb9f8fa3e5a37fa6b2
Tom <Tom@Tom_PC>
2016-08-02 17:05:51 +0200

    add unit tests

--- test/org/worldgrower/actions/UTestBuildSacrificialAltarAction.java
+++ test/org/worldgrower/actions/UTestBuildSacrificialAltarAction.java
@@ -65,6 +65,15 @@ public class UTestBuildSacrificialAltarAction {
 		assertEquals(true, Actions.BUILD_SACRIFICAL_ALTAR_ACTION.isActionPossible(performer, target, Args.EMPTY, world));
 	}
 	
+	@Test
+	public void testDistance() {
+		World world = new WorldImpl(1, 1, null, null);
+		WorldObject performer = createPerformer(2);
+		WorldObject target = createPerformer(3);
+		
+		assertEquals(0, Actions.BUILD_SACRIFICAL_ALTAR_ACTION.distance(performer, target, Args.EMPTY, world));
+	}
+	
 	private WorldObject createPerformer(int id) {
 		WorldObject performer = TestUtils.createSkilledWorldObject(id, Constants.INVENTORY, new WorldObjectContainer());
 		performer.setProperty(Constants.X, 0);
