ce634f306c239a121d368c133bff50f0aa4bfbab
Tom <Tom@Tom_PC>
2016-12-03 09:41:00 +0100

    cleanup imports

--- src/org/worldgrower/gui/ImageInfoReader.java
+++ src/org/worldgrower/gui/ImageInfoReader.java
@@ -19,7 +19,6 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
