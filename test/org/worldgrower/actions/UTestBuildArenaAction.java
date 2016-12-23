d0af3399ef1af0297bdfa4a2398487069bc4547c
Tom <Tom@Tom_PC>
2016-12-23 14:26:32 +0100

    fix unit tests

--- test/org/worldgrower/actions/UTestBuildArenaAction.java
+++ test/org/worldgrower/actions/UTestBuildArenaAction.java
@@ -42,7 +42,7 @@ public class UTestBuildArenaAction {
 	
 	@Test
 	public void testIsValidTarget() {
-		World world = new WorldImpl(10, 10, null, null);
+		World world = new WorldImpl(15, 15, null, null);
 		WorldObject performer = createPerformer(2);
 		WorldObject target = TestUtils.createWorldObject(0, 0, 1, 1);
 		assertEquals(true, Actions.BUILD_ARENA_ACTION.isValidTarget(performer, target, world));
