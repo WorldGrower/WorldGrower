a0b77f2720ba7cd829effe8727774f779bd39dbe
Tom <Tom@Tom_PC>
2017-01-06 17:23:54 +0100

    expand DocumentationGenerator

--- src/org/worldgrower/documentation/DocumentationGenerator.java
+++ src/org/worldgrower/documentation/DocumentationGenerator.java
@@ -17,11 +17,17 @@ package org.worldgrower.documentation;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 
+import org.worldgrower.Constants;
 import org.worldgrower.actions.Actions;
 import org.worldgrower.actions.magic.MagicSpell;
+import org.worldgrower.attribute.IntProperty;
+import org.worldgrower.condition.Condition;
 import org.worldgrower.generator.Item;
 import org.worldgrower.generator.ItemType;
 import org.worldgrower.gui.ImageIds;
@@ -36,48 +42,123 @@ public class DocumentationGenerator {
 
 		generateMagicSpellOverview(outputDir, imageInfoReader);
 		generateToolsOverview(outputDir, imageInfoReader);
+		generateEquipmentOverview(outputDir, imageInfoReader);
+		generateDiseasesOverview(outputDir, imageInfoReader);
 	}
 
 	private static void generateMagicSpellOverview(File outputDir, ImageInfoReader imageInfoReader) {
-		StringBuilder spellsHtmlBuilder = new StringBuilder("<html>");
-		spellsHtmlBuilder.append("<table>");
-		spellsHtmlBuilder.append("<tr><th>Icon</th><th>Name</th><th>Magic School</th><th>Description</th></tr>");
+		File outputFile = new File(outputDir, "_gen_spells.html");
+		List<String> headerFields = Arrays.asList("Icon", "Name", "Magic School", "Description");
+		List<List<String>> tableValues = new ArrayList<List<String>>();
 		for(MagicSpell magicSpell : Actions.getMagicSpells()) {
+			List<String> tableRow = new ArrayList<>();
 			String magicSpellFilename = "_gen_" + magicSpell.getClass().getName() + ".png";
 			saveImage(magicSpell.getImageIds(null) , imageInfoReader, new File(outputDir, magicSpellFilename));
-			spellsHtmlBuilder.append("<tr>");
-			spellsHtmlBuilder.append("<td><img src=\"").append(magicSpellFilename).append("\"></td>");
-			spellsHtmlBuilder.append("<td>").append(magicSpell.getSimpleDescription()).append("</td>");
-			spellsHtmlBuilder.append("<td>").append(magicSpell.getSkill().getName()).append("</td>");
-			spellsHtmlBuilder.append("<td>").append(magicSpell.getDescription()).append("</td>");
-			spellsHtmlBuilder.append("</tr>");
+			
+			tableRow.add(imageTag(magicSpellFilename));
+			tableRow.add(magicSpell.getSimpleDescription());
+			tableRow.add(magicSpell.getSkill().getName());
+			tableRow.add(magicSpell.getDescription());
+			
+			tableValues.add(tableRow);
 		}
-		spellsHtmlBuilder.append("</table>");
-		spellsHtmlBuilder.append("</html>");
 		
-		FileUtils.writeTextFile(new File(outputDir, "_gen_spells.html"), spellsHtmlBuilder.toString());
+		createHtmlFile(outputFile, imageInfoReader, headerFields, tableValues);
 	}
 	
 	private static void generateToolsOverview(File outputDir, ImageInfoReader imageInfoReader) {
-		StringBuilder toolsHtmlBuilder = new StringBuilder("<html>");
-		toolsHtmlBuilder.append("<table>");
-		toolsHtmlBuilder.append("<tr><th>Icon</th><th>Name</th></tr>");
+		File outputFile = new File(outputDir, "_gen_tools.html");
+		List<String> headerFields = Arrays.asList("Icon", "Name");
+		List<List<String>> tableValues = new ArrayList<List<String>>();
 		for(Item item : Item.values()) {
-			if (item.getItemType() == ItemType.TOOL) {
+			if (item.getItemType() == ItemType.TOOL || item.generate(1f).hasProperty(Constants.WOOD_CUTTING_QUALITY)) {
+				List<String> tableRow = new ArrayList<>();
 				String toolFilename = "_gen_" + item.getDescription() + ".png";
 				saveImage(item.getImageId(), imageInfoReader, new File(outputDir, toolFilename));
-				toolsHtmlBuilder.append("<tr>");
-				toolsHtmlBuilder.append("<td><img src=\"").append(toolFilename).append("\"></td>");
-				toolsHtmlBuilder.append("<td>").append(item.getDescription()).append("</td>");
-				toolsHtmlBuilder.append("</tr>");
+				
+				tableRow.add(imageTag(toolFilename));
+				tableRow.add(item.getDescription());
+
+				tableValues.add(tableRow);
 			}
 		}
-		toolsHtmlBuilder.append("</table>");
-		toolsHtmlBuilder.append("</html>");
-		
-		FileUtils.writeTextFile(new File(outputDir, "_gen_tools.html"), toolsHtmlBuilder.toString());
+		createHtmlFile(outputFile, imageInfoReader, headerFields, tableValues);
 	}
 
+	private static void generateEquipmentOverview(File outputDir, ImageInfoReader imageInfoReader) {
+		File outputFile = new File(outputDir, "_gen_equipment.html");
+		List<String> headerFields = Arrays.asList("Icon", "Name", "Damage", "Armor");
+		List<List<String>> tableValues = new ArrayList<List<String>>();
+		for(Item item : Item.values()) {
+			if (item.getItemType() == ItemType.ARMOR || item.getItemType() == ItemType.WEAPON) {
+				List<String> tableRow = new ArrayList<>();
+				String equipmentFilename = "_gen_" + item.getDescription() + ".png";
+				saveImage(item.getImageId(), imageInfoReader, new File(outputDir, equipmentFilename));
+
+				tableRow.add(imageTag(equipmentFilename));
+				tableRow.add(item.getDescription());
+				tableRow.add(getPropertyValue(item, Constants.DAMAGE));
+				tableRow.add(getPropertyValue(item, Constants.ARMOR));
+				
+				tableValues.add(tableRow);
+			}
+		}
+		createHtmlFile(outputFile, imageInfoReader, headerFields, tableValues);
+	}
+	
+	private static void generateDiseasesOverview(File outputDir, ImageInfoReader imageInfoReader) {
+		File outputFile = new File(outputDir, "_gen_diseases.html");
+		List<String> headerFields = Arrays.asList("Icon", "Name", "Description");
+		List<List<String>> tableValues = new ArrayList<List<String>>();
+		for(Condition condition : Condition.ALL_CONDITIONS) {
+			if (condition.isDisease()) {
+				List<String> tableRow = new ArrayList<>();
+				String conditionFilename = "_gen_" + condition.getDescription() + ".png";
+				saveImage(condition.getImageIds(), imageInfoReader, new File(outputDir, conditionFilename));
+
+				tableRow.add(imageTag(conditionFilename));
+				tableRow.add(condition.getDescription());
+				tableRow.add(condition.getLongerDescription());
+				
+				tableValues.add(tableRow);
+			}
+		}
+		createHtmlFile(outputFile, imageInfoReader, headerFields, tableValues);
+	}
+
+	private static String getPropertyValue(Item item, IntProperty property) {
+		String value = "";
+		if (item.generate(1f).hasProperty(property)) {
+			value = item.generate(1f).getProperty(property).toString();	
+		}
+		return value;
+	}
+	
+	private static String imageTag(String imageSource) {
+		return "<img src=\"" + imageSource + "\">";
+	}
+	
+	private static void createHtmlFile(File outputFile, ImageInfoReader imageInfoReader, List<String> headerFields, List<List<String>> tableValues) {
+		StringBuilder htmlBuilder = new StringBuilder("<html>");
+		htmlBuilder.append("<table>");
+		htmlBuilder.append("<tr>");
+		for(String headerField : headerFields) {
+			htmlBuilder.append("<th>").append(headerField).append("</th>");
+		}
+		htmlBuilder.append("</tr>");
+		for(List<String> tableRow : tableValues) {
+			htmlBuilder.append("<tr>");
+			for(String tableValue : tableRow) {
+				htmlBuilder.append("<td>").append(tableValue).append("</td>");
+			}
+			htmlBuilder.append("</tr>");
+		}
+		htmlBuilder.append("</table>");
+		htmlBuilder.append("</html>");
+		
+		FileUtils.writeTextFile(outputFile, htmlBuilder.toString());		
+	}
+	
 	private static void saveImage(ImageIds imageIds, ImageInfoReader imageInfoReader, File outputFile) {
 		try {
 		    BufferedImage bi = (BufferedImage) imageInfoReader.getImage(imageIds, null);
