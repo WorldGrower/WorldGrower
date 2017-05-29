fa8b7c9e764ed7f017d8e79d12191d4a1a138761
Tom <Tom@Tom_PC>
2017-05-29 18:29:20 +0200

    fix unit test

--- test/org/worldgrower/goal/UTestDeathReasonPropertyUtils.java
+++ test/org/worldgrower/goal/UTestDeathReasonPropertyUtils.java
@@ -57,6 +57,6 @@ public class UTestDeathReasonPropertyUtils {
 	
 	@Test
 	public void testGetAllDeathReasons() {
-		assertEquals(17, DeathReasonPropertyUtils.getAllDeathReasons().size());
+		assertEquals(18, DeathReasonPropertyUtils.getAllDeathReasons().size());
 	}
 }
