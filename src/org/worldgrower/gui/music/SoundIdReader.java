3d9d57c67acfb3b0db93120506cf5382c03c3670
Tom <Tom@Tom_PC>
2017-10-10 10:02:18 +0200

    add chicken sound effect

--- src/org/worldgrower/gui/music/SoundIdReader.java
+++ src/org/worldgrower/gui/music/SoundIdReader.java
@@ -90,7 +90,7 @@ public class SoundIdReader {
 		readSound(SoundIds.CURSE4, "/sound/curse48bit.wav.gz");
 		readSound(SoundIds.MAGIC_DROP, "/sound/magicdrop8bit.wav.gz");
 		readSound(SoundIds.PLANT_GROWTH, "/sound/qubodupGrowthSpell01_8bit.wav.gz");
-		
+		readSound(SoundIds.CHICKEN, "/sound/192035__dann93__chickens.wav.gz");
 	}
 
 	public SoundIdReader(SoundOutput soundOutput, boolean enabled) throws SoundException {
